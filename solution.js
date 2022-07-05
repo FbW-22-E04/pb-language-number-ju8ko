5b4444190151aed2a10672550720c1be7bde72a0
ju8ko <julia.x.kobzar@gmail.com>
2022-07-05 13:34:19 +0200

    Exercise is done

---
+++ solution.js
@@ -0,0 +1,13 @@
+//1
+let introSentence = "Ex.1: hello, my name is Julia and I am ";
+let age = 30;
+console.log(introSentence + age);
+//The + operator concatenating string values together
+
+//2
+let integer = "1005";
+console.log(parseInt(integer));
+
+//3
+let float = "10.05";
+console.log(parseFloat(float));
